44e1dc95a3499355d352b6cab6c2ddd97b481cb4
Swathisathyan <82638675+Swathisathyan@users.noreply.github.com>
2021-06-09 20:56:32 +0530

    Create sizec++.cc

---
+++ sizec++.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"size of integer is:" <<sizeof(int); cout<<" byte\n";
+cout<<"size of float is:" <<sizeof(float); cout<<" byte\n";
+cout<<"size of double  is:" <<sizeof(double); cout<<" byte\n";
+cout<<"size of char is:" <<sizeof(char); cout<<" byte\n";
+    return 0;
+}
